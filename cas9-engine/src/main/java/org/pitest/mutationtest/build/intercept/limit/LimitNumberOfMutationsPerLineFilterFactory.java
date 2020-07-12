f0a672f2ab880a188e48dd3cb29f0e3f8ba6367c
Glauco Aquino <glauco.aquino@aurea.com>
2020-07-12 09:15:56 -0300

    refact(engine) add op priority to limit filter

--- cas9-engine/src/main/java/org/pitest/mutationtest/build/intercept/limit/LimitNumberOfMutationsPerLineFilterFactory.java
+++ cas9-engine/src/main/java/org/pitest/mutationtest/build/intercept/limit/LimitNumberOfMutationsPerLineFilterFactory.java
@@ -1,13 +1,15 @@
 package org.pitest.mutationtest.build.intercept.limit;
 
+import static java.util.Arrays.asList;
+import static java.util.Collections.unmodifiableList;
 import static java.util.Comparator.comparingInt;
 import static java.util.stream.Collectors.groupingBy;
 import static java.util.stream.Collectors.toList;
 
 import java.util.Collection;
 import java.util.Comparator;
+import java.util.List;
 import lombok.Value;
-import lombok.val;
 import org.pitest.bytecode.analysis.ClassTree;
 import org.pitest.mutationtest.build.InterceptorParameters;
 import org.pitest.mutationtest.build.InterceptorType;
@@ -15,7 +17,7 @@ import org.pitest.mutationtest.build.MutationInterceptor;
 import org.pitest.mutationtest.build.MutationInterceptorFactory;
 import org.pitest.mutationtest.engine.Mutater;
 import org.pitest.mutationtest.engine.MutationDetails;
-import org.pitest.mutationtest.engine.cas9.config.Mutator;
+import org.pitest.mutationtest.engine.cas9.config.Cas9Mutators;
 import org.pitest.mutationtest.engine.gregor.MethodMutatorFactory;
 import org.pitest.plugin.Feature;
 import org.pitest.plugin.FeatureParameter;
@@ -27,6 +29,10 @@ public class LimitNumberOfMutationsPerLineFilterFactory implements MutationInter
       .withDescription("Integer value for maximum mutations to create per line");
 
   private static final Integer DEFAULT_LIMIT = 1;
+
+  private static final List<String> OPERATOR_PRIORITY = unmodifiableList(asList("LCR", "UOI", "ROR", "SBR", "AOR"));
+
+  private static final List<String> ORDERED_MUTATORS = getMutatorsOrderedByPriority();
 
   @Override
   public MutationInterceptor createInterceptor(InterceptorParameters params) {
@@ -47,6 +53,14 @@ public class LimitNumberOfMutationsPerLineFilterFactory implements MutationInter
     return "Max mutations per line limit";
   }
 
+  private static List<String> getMutatorsOrderedByPriority() {
+    return OPERATOR_PRIORITY.stream()
+        .map(Cas9Mutators::fromOperator)
+        .flatMap(Collection::stream)
+        .map(MethodMutatorFactory::getGloballyUniqueId)
+        .collect(toList());
+  }
+
   @Value(staticConstructor = "of")
   static class LimitNumberOfMutationsPerLineFilter implements MutationInterceptor {
 
@@ -62,17 +76,13 @@ public class LimitNumberOfMutationsPerLineFilterFactory implements MutationInter
 
     @Override
     public Collection<MutationDetails> intercept(Collection<MutationDetails> mutations, Mutater mutater) {
-      val orderedOperators = Mutator.all().stream()
-          .map(MethodMutatorFactory::getGloballyUniqueId)
-          .collect(toList());
-
-      Comparator<? super MutationDetails> byMutationPriority =
-          comparingInt(detail -> orderedOperators.indexOf(detail.getMutator()));
+      Comparator<? super MutationDetails> byMutatorPriority =
+          comparingInt(detail -> ORDERED_MUTATORS.indexOf(detail.getMutator()));
 
       return mutations.stream()
           .collect(groupingBy(MutationDetails::getLineNumber))
           .values().stream()
-          .peek(details -> details.sort(byMutationPriority))
+          .peek(details -> details.sort(byMutatorPriority))
           .map(details -> details.get(0))
           .collect(toList());
     }
